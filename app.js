b0fd3aca217db42fc6921d1ae3329401f2b7dac5
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-09-15 15:20:27 -0400

    init

---
+++ app.js
@@ -0,0 +1,4 @@
+import { Display, javaT } from "./mobile.js";
+const d = new Display();
+d.show();
+javaT();
---
+++ mobile.js
@@ -0,0 +1,9 @@
+class Display {
+  show() {
+    console.log("hello world");
+  }
+}
+function javaT() {
+  console.log("welcome to comp 229");
+}
+export { Display, javaT };
